4d05fc6282f3a838fde2276c08361ad6392cf837
Guntereno <GunterPete@hotmail.com>
2019-05-27 19:20:46 +0100

    Added initial project.
    - Contains a Generator with a view object
    - Added initial .gitignore

---
+++ Assets/Generator/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Generator
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField]
+        private View ViewPrefab = null;
+
+        private Transform m_rootNode;
+        private Mesh m_mesh;
+        private View m_view;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_mesh = new Mesh();
+            m_view = GameObject.Instantiate(ViewPrefab);
+            OnMeshUpdated(m_mesh);
+        }
+
+        private void OnMeshUpdated(Mesh mesh)
+        {
+            m_view.MeshUpdated(mesh);
+        }
+    }
+}
---
+++ Assets/Generator/View.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Generator
+{
+    class View : MonoBehaviour
+    {
+        private Mesh m_mesh;
+        private Transform m_resultsRoot = null;
+
+        public void MeshUpdated(Mesh mesh)
+        {
+            // If the mesh has changed...
+            if(m_mesh != mesh)
+            {
+                // Rebuild the scene heirarchy
+                m_mesh = mesh;
+                if(m_resultsRoot != null)
+                {
+                    GameObject.Destroy(m_resultsRoot);
+                }
+            }
+        }
+    }
+}
